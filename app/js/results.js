a93da46c88bc1b1dfc0786810606c4931ffedc0d
Curt <curtis.merrell@gmail.com>
2015-12-01 11:26:53 -0700

    Added results page

---
+++ app/js/results.js
@@ -0,0 +1,12 @@
+var Results = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h1>Results</h1>
+        <p>Your info here!</p>
+      </div>
+    );
+  }
+});
+
+export default Results;
