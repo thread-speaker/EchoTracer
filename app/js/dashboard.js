c367ebb623fc3ddceaa639a6426419571690d178
Aaron Monson <aaron@lmonson.com>
2015-12-03 21:26:11 -0700

    started work on geolookups

--- app/js/dashboard.js
+++ app/js/dashboard.js
@@ -1,9 +1,24 @@
 var Dashboard = React.createClass({
+
+  getLocation: function() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        alert(position.coords.latitude + ", " + position.coords.longitude);
+      });
+    }
+    else {
+      alert("Geolocation lookup failed!");
+    }
+  },
+
   render: function() {
     return (
       <div>
         <h1>Dashboard</h1>
-        <p> Content goes here? </p>
+        <div className="save-location">
+          <div className="button" onMouseDown={this.getLocation}>Record My Location</div>
+          <p id="location-message">Hello World!</p>
+        </div>
       </div>
     );
   }
